de4c9572a98375d6333bb270f8eed3d8630b2623
scatowl <scatowl@qq.com>
2021-03-26 14:43:19 +0800

    删除完成任务，避免重复上传

--- main.py
+++ main.py
@@ -16,9 +16,13 @@ if __name__ == "__main__":
             ggn_links = games.read().splitlines()
     else:
         ggn_links = [ggn_link]
-    for ggn_link in ggn_links:
+    for ggn_link in ggn_links.copy():
         ggn = site_api.GGnApi(ggn_link)
         ggn_info = ggn.worker()
         pter = site_api.PTerApi(ggn_info)
         pter.worker()
+        ggn_links.remove(ggn_link)
+    with open('ggn_links.txt','w') as gls:
+        for link in ggn_links:
+            gls.write(link+'\n')
     input('上传完毕，输入任意字符退出')
