69529ba44fddb8ea8c86a8aef9989044f96ffefe
scatowl <scatowl@qq.com>
2021-03-26 13:56:13 +0800

    批量模式

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ if __name__ == "__main__":
         cookies = {"ggn":ggn_cookie,"pter":pter_cookie}
         with open('cookies.json','w') as coo:
             json.dump(cookies,coo)
-    while True:
-        ggn_link = input('请输入一个GGn种子下载连接：')
+    ggn_link = input('请输入一个GGn种子下载连接或者直接回车开始扫描\'games.txt\'文件：')
+    if ggn_link == '':
+        with open('ggn_links.txt') as games:
+            ggn_links = games.read().splitlines()
+    else:
+        ggn_links = [ggn_link]
+    for ggn_link in ggn_links:
         ggn = site_api.GGnApi(ggn_link)
         ggn_info = ggn.worker()
         pter = site_api.PTerApi(ggn_info)
