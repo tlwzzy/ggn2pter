d3f4fc28176f611fedd7e6e0fc5043deb45b19c4
tlwzzy <36352458+tlwzzy@users.noreply.github.com>
2021-05-14 09:25:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def initial():
             passkey = input('请输入猫站passkey：')
             anonymous = input('是否匿名发布(yes/no)：')
             torrent_dir = input('请输入种子下载路径，默认为当前目录下的torrents文件夹:')
-            elite_gamer = input('是否为GGn elite gamer 及以上(yes/noe默认为是)')
+            elite_gamer = input('是否为GGn elite gamer 及以上(yes/no 默认为是)')
             if torrent_dir == '':
                 torrent_dir = 'torrents'
             if elite_gamer != 'no':
